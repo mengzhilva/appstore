7dd7de2f876a3bf8e4c68c2893925a66afe5f16e
lvdingguo <5480964+lvdingguo@user.noreply.gitee.com>
2022-07-25 17:31:42 +0800

    addfile

--- src/appstore.php
+++ src/appstore.php
@@ -8,6 +8,20 @@
 
 /*
  * 使用方法
+ * 
+安装：
+composer require ldg/ldgappstore
+实例化
+$store = new appstore('cn','zh');
+获取排名列表
+$store->search('abc');
+获取app详情
+$store->appinfo(appid);
+获取榜单
+$store->top();
+获取评论
+$store->reviews(appid);
+
 
 *
 * */ 
